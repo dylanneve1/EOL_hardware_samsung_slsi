79a9ce8544f8c8efb8c4242081666d9835ef8a73
HyunKyung Kim <hk310.kim@samsung.com>
2020-09-18 18:31:35 +0900

    libhwc2.1: Fix bug in setting return parameter of getHdrCapabilities
    Signed-off-by: HyunKyung Kim <hk310.kim@samsung.com>
    Change-Id: I0c4faf350eb1ea83af40f32a1c60959c8614a392

--- graphics/base/libhwc2.1/libdevice/ExynosDisplay.cpp
+++ graphics/base/libhwc2.1/libdevice/ExynosDisplay.cpp
@@ -4821,7 +4821,7 @@ int32_t ExynosDisplay::getHdrCapabilities(uint32_t* outNumTypes,
             return HWC2_ERROR_BAD_CONFIG;
     }
 
-    *outMaxLuminance = mHdrTypeNum;
+    *outMaxLuminance = mMaxLuminance;
     *outMaxAverageLuminance = mMaxAverageLuminance;
     *outMinLuminance = mMinLuminance;
 
