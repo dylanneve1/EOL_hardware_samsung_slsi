516e5145e43582f7cc899963da35e81fdde1478a
Long Ling <longling@google.com>
2021-01-13 21:01:00 -0800

    libhwc2.1 Update enum MPP_SOURCE type
    remove MPP_SOURCE_NO_TYPE and add MPP_SOURCE_MAX
    
    Bug: 168225512
    Change-Id: I1255b7fc4f75f172a16f0ced3bc53bd9f1dc149e

--- graphics/base/libhwc2.1/libresource/ExynosMPP.cpp
+++ graphics/base/libhwc2.1/libresource/ExynosMPP.cpp
@@ -89,7 +89,7 @@ void dump(const restriction_size_t &restrictionSize, String8 &result) {
 }
 
 ExynosMPPSource::ExynosMPPSource()
-    : mSourceType(MPP_SOURCE_NO_TYPE),
+    : mSourceType(MPP_SOURCE_MAX),
     mSource(NULL),
     mOtfMPP(NULL),
     mM2mMPP(NULL)
