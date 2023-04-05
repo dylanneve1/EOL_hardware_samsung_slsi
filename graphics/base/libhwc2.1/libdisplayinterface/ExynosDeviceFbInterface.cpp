slogar25/aosp_hardware_google_graphics_common

libhwc2.1: Optimize source code checking duplicate attr
Change-Id: I46be4f4e07fe7009e9beed83ec77d9ea5780ca72
Signed-off-by: HyunKyung Kim <hk310.kim@samsung.com>

libhwc2.1/libdisplayinterface/ExynosDeviceFbInterface.cpp
@@ -20,6 +20,7 @@
 #include "ExynosExternalDisplayModule.h"
 #include "ExynosHWCHelper.h"
 #include "ExynosHWCDebug.h"
+#include <unordered_set>
 
 extern update_time_info updateTimeInfo;
 extern feature_support_t feature_table[];
@@ -253,17 +254,14 @@ int32_t ExynosDeviceFbInterface::makeDPURestrictions() {
     restriction_key_t queried_format_table[1024];
 
     /* Check attribute overlap */
-    for (i = 0; i < dpuInfo->dpp_cnt; i++){
-        for (j = 0; j < dpuInfo->dpp_cnt; j++){
-            if (i >= j) continue;
-            dpp_ch_restriction r1 = dpuInfo->dpp_ch[i];
-            dpp_ch_restriction r2 = dpuInfo->dpp_ch[j];
-            /* If attribute is same, will not be added to table */
-            if (r1.attr == r2.attr) {
-                mDPUInfo.overlap[j] = true;
-            }
-        }
-        HDEBUGLOGD(eDebugDefault, "Index : %d, overlap %d", i, mDPUInfo.overlap[i]);
+    std::unordered_set<unsigned long> attrs;
+    for (size_t i = 0; i < dpuInfo->dpp_cnt; ++i) {
+        const dpp_ch_restriction &r = dpuInfo->dpp_ch[i];
+        if (attrs.count(r.attr))
+            mDPUInfo.overlap[i] = true;
+        else
+            attrs.insert(r.attr);
+        HDEBUGLOGD(eDebugDefault, "Index : %zu, overlap %d", i, mDPUInfo.overlap[i]);
     }
 
     for (i = 0; i < dpuInfo->dpp_cnt; i++){

dylanneve1/EOL_hardware_samsung_slsi

graphics/base/libhwc2.1/libdisplayinterface/ExynosDeviceFbInterface.cpp
@@ -5,6 +5,7 @@
 #include "ExynosHWCHelper.h"
 #include "ExynosHWCDebug.h"
 #include "ExynosResourceRestriction.h"
+#include <unordered_set>
 
 extern update_time_info updateTimeInfo;
 #ifndef USE_MODULE_ATTR
@@ -203,17 +204,14 @@ int32_t ExynosDeviceFbInterface::makeDPURestrictions() {
     restriction_key_t queried_format_table[1024];
 
     /* Check attribute overlap */
-    for (i = 0; i < dpuInfo->dpp_cnt; i++){
-        for (j = 0; j < dpuInfo->dpp_cnt; j++){
-            if (i >= j) continue;
-            dpp_ch_restriction r1 = dpuInfo->dpp_ch[i];
-            dpp_ch_restriction r2 = dpuInfo->dpp_ch[j];
-            /* If attribute is same, will not be added to table */
-            if (r1.attr == r2.attr) {
-                mDPUInfo.overlap[j] = true;
-            }
-        }
-        HDEBUGLOGD(eDebugAttrSetting, "Index : %d, overlap %d", i, mDPUInfo.overlap[i]);
+    std::unordered_set<unsigned long> attrs;
+    for (size_t i = 0; i < dpuInfo->dpp_cnt; ++i) {
+        const dpp_ch_restriction &r = dpuInfo->dpp_ch[i];
+        if (attrs.count(r.attr))
+            mDPUInfo.overlap[i] = true;
+        else
+            attrs.insert(r.attr);
+        HDEBUGLOGD(eDebugAttrSetting, "Index : %zu, overlap %d", i, mDPUInfo.overlap[i]);
     }
 
     for (i = 0; i < dpuInfo->dpp_cnt; i++){