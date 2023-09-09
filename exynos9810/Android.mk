9e83598424758b75e58d794ceff1087fb0a90cc0
Dylan Neve <dylanneve1@gmail.com>
2023-09-09 16:20:20 +0100

    exynos9810: exclude audio, camera, gatekeeper and keymaster
    Probably not going to work properly out the box

--- exynos9810/Android.mk
+++ exynos9810/Android.mk
@@ -15,17 +15,17 @@
 #
 
 ifeq ($(TARGET_SOC_BASE), exynos9810)
- exynos9810_dirs := \
- 	libkeymaster \
-	libgatekeeper \
-	libcamera3
+# exynos9810_dirs := \
+# 	libkeymaster \
+#	libgatekeeper \
+#	libcamera3
 
 ifeq ($(BOARD_USE_COMMON_AUDIOHAL), true)
 ifeq ($(BOARD_USE_AUDIOHAL), true)
-exynos9810_dirs += libaudio
+#exynos9810_dirs += libaudio
 endif
 endif
 
-include $(call all-named-subdir-makefiles,$(exynos9810_dirs))
+#include $(call all-named-subdir-makefiles,$(exynos9810_dirs))
 
 endif
